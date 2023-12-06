d545298a8f21f7f94d3e81f3e1c941da94b11a43
Hojiakbar5119 <Abdulhakimovhojiakbar123@gmail.com>
2023-12-07 03:57:17 +0500

    typescript

---
+++ 1/index.js
@@ -0,0 +1,16 @@
+"use strict";
+// console.log("Salom dunyo");
+// console.log("mening Ismim Hojiakbar");
+// alert("Ha shu ekanda")
+// let a =12;
+// let a:string = "web"
+// console.log(a);
+// let b:null = null
+// console.log(b);
+// function Pow(x:number, y:number){
+//     return x**y
+// }
+// console.log(Pow(2,4));
+//! Arrow function 
+const Pow = (x, y) => x ** y;
+console.log(Pow(2, 4));
---
+++ 1/index.ts
@@ -0,0 +1,27 @@
+// console.log("Salom dunyo");
+// console.log("mening Ismim Hojiakbar");
+// alert("Ha shu ekanda")
+
+// let a =12;
+// let a:string = "web"
+
+// console.log(a);
+
+
+// let b:null = null
+// console.log(b);
+
+
+
+
+// function Pow(x:number, y:number){
+//     return x**y
+// }
+// console.log(Pow(2,4));
+
+
+//! Arrow function 
+
+const Pow = (x:number,y:number):number=>x**y
+console.log(Pow(2,4));
+ 
